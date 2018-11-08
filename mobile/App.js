29e421507c0dcef04043e0c357e8033ea540ebc0
Francis Granger <franki_le_pro@hotmail.com>
2018-11-08 15:56:36 -0500

    init with native base

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Constants } from "expo";
+import { Platform } from "react-native";
+import { Container, Header, Content, Button, Text } from 'native-base';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+    this.setState({ loading: false });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Salut le monde</Text>
-      </View>
+      <>
+        {
+          !this.state.loading &&
+          <Container style={{ paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight }}>
+            <Header />
+            <Content>
+              <Button>
+                <Text>Click Me!</Text>
+              </Button>
+            </Content>
+          </Container>
+        }
+      </>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
