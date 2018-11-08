f9e964a3133caccf7c11b0ebeab232ff3fb3b7ce
Francis Granger <franki_le_pro@hotmail.com>
2018-11-08 17:55:19 -0500

    add dumb tabs

--- mobile/App.js
+++ mobile/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Constants } from "expo";
 import { Platform } from "react-native";
-import { Container, Header, Content, Button, Text } from 'native-base';
-import {observable} from 'mobx';
-import {observer} from 'mobx-react/native'
+import { observable } from 'mobx';
+import { observer } from 'mobx-react/native'
+import { Container, Header, Tab, Tabs, Left, Button, Icon, Body, Title, Right } from 'native-base';
+
+import Lists from './components/lists';
+import Main from './components/main';
+import Shipper from './components/shipper';
 
 @observer
 export default class App extends React.Component {
   @observable loading = true;
-  
+
   constructor(props) {
     super(props);
   }
@@ -27,12 +31,29 @@ export default class App extends React.Component {
         {
           !this.loading &&
           <Container style={{ paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight }}>
-            <Header />
-            <Content>
-              <Button>
-                <Text>Click Me!</Text>
-              </Button>
-            </Content>
+            <Header hasTabs>
+              <Left>
+                <Button transparent onPress={() => this.props.navigation.goBack()}>
+                  <Icon name="arrow-back" />
+                </Button>
+              </Left>
+              <Body>
+                <Title> Basic Tabs</Title>
+              </Body>
+              <Right />
+            </Header>
+
+            <Tabs>
+              <Tab heading="Tab1">
+                <Lists />
+              </Tab>
+              <Tab heading="Tab2">
+                <Main />
+              </Tab>
+              <Tab heading="Tab3">
+                <Shipper />
+              </Tab>
+            </Tabs>
           </Container>
         }
       </>
