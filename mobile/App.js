b78a529143da0982f9dc4b527ce81c1b15ae851d
Francis Granger <franki_le_pro@hotmail.com>
2018-11-08 17:08:04 -0500

    add mobx support

--- mobile/App.js
+++ mobile/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Constants } from "expo";
 import { Platform } from "react-native";
 import { Container, Header, Content, Button, Text } from 'native-base';
+import {observable} from 'mobx';
+import {observer} from 'mobx-react/native'
 
+@observer
 export default class App extends React.Component {
+  @observable loading = true;
+  
   constructor(props) {
     super(props);
-    this.state = { loading: true };
   }
 
   async componentWillMount() {
@@ -14,14 +18,14 @@ export default class App extends React.Component {
       'Roboto': require('native-base/Fonts/Roboto.ttf'),
       'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
     });
-    this.setState({ loading: false });
+    this.loading = false;
   }
 
   render() {
     return (
       <>
         {
-          !this.state.loading &&
+          !this.loading &&
           <Container style={{ paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight }}>
             <Header />
             <Content>
