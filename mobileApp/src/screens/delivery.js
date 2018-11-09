d2bc35b369a77ed0f6e00d041825f2ca46cbdef5
Sébastien Labine <sebastienlabine@MacBook-Pro-de-Sebastien.local>
2018-11-09 00:23:21 -0500

    List of stores available

--- mobileApp/src/screens/delivery.js
+++ mobileApp/src/screens/delivery.js
@@ -1,16 +1,29 @@
-import React, { Component, View } from "react";
+import React, { Component, View, TouchableOpacity } from "react";
 import { Content, Card, CardItem, Text, Body, Thumbnail, Grid, Col, Row } from "native-base";
+import listsStores from "./listsStore";
+import { observable } from "mobx";
+import { observer } from "mobx-react/native";
 
+@observer
 export default class Delivery extends Component {
+
+  @observable
+  people = [];
+
+
+  addPeopleFromStore (store) {
+    this.people = listsStores.deliveries[store];
+  }
+  
   render() {
-    const uris = {
-      costco: "https://cdn.iconscout.com/icon/free/png-256/costco-282448.png",
-      iga: "https://archive.li/A4yBK/ab49db9d993e090d7e5afabcbb715e8bbf113d84.gif",
-      metro: "https://www.metro.ca/userfiles/image/publicite/logo-metro.png",
-      walmart: "https://cdn.iconscout.com/icon/free/png-256/walmart-282199.png",
-      superC: "https://www.placedeville.com/app/uploads/sites/11/2018/04/SuperC_Web.png",
-      maxi: "https://www.circulaire-en-ligne.ca/wp-content/uploads/Circulaire-en-ligne-Maxi-et-Maxi-Cie.jpg",
-    };
+    const uris = [
+      { store: "costco", url: "https://cdn.iconscout.com/icon/free/png-256/costco-282448.png" },
+      { store: "iga", url: "https://archive.li/A4yBK/ab49db9d993e090d7e5afabcbb715e8bbf113d84.gif" },
+      { store: "metro", url: "https://www.metro.ca/userfiles/image/publicite/logo-metro.png" },
+      { store: "walmart", url: "https://cdn.iconscout.com/icon/free/png-256/walmart-282199.png" },
+      { store: "superC", url: "https://www.placedeville.com/app/uploads/sites/11/2018/04/SuperC_Web.png" },
+      { store: "maxi", url: "https://www.circulaire-en-ligne.ca/wp-content/uploads/Circulaire-en-ligne-Maxi-et-Maxi-Cie.jpg" }];
+
     return (
       <Content padder style={{ marginTop: 0 }}>
         <Card style={{ flex: 0 }}>
@@ -22,48 +35,47 @@ export default class Delivery extends Component {
             </Body>
           </CardItem>
         </Card>
-
         <Grid>
-          <Row>
           <Col size={2}>
-          <Card style={{ flex: 0 }}>
-            <CardItem>
-              <Body>
-                <Text>
-                  DSFD
-                </Text>
-              </Body>
-            </CardItem>
-          </Card>
+            {uris.filter((value, index) => index % 2 === 0).map((value, index) =>
+              <Row
+                key={index}>
+                <Card>
+                  <CardItem button onPress={() => this.addPeopleFromStore(value.store)}>
+                    <Thumbnail medium
+                      source={value}
+                    >
+                    </Thumbnail>
+                  </CardItem>
+                </Card>
+              </Row>
+            )}
           </Col>
           <Col size={2}>
-          <Card style={{ flex: 0 }}>
-            <CardItem>
-              <Body>
-                <Text>
-                  Text
-                </Text>
-              </Body>
-            </CardItem>
-          </Card>
-          </Col>
-          </Row>
-        </Grid>
-        {Object.entries(uris).map(([key, value]) =>
-          <Card key={key} style={{ flex: 0 }}>
-            <CardItem>
-              <Thumbnail small
-                source={{ uri: value }}>
-              </Thumbnail>
-              <Body>
-                <Text>
-                  {key}
-                </Text>
-              </Body>
-            </CardItem>
-          </Card>
-        )}
+            {uris.filter((value, index) => index % 2 !== 0).map((value, index) =>
+              <Row key={index}>
+                <Card>
+                  <CardItem button onPress={() => this.addPeopleFromStore(value.store)}>
+                    <Thumbnail medium
+                      source={value}>
 
+                    </Thumbnail>
+                  </CardItem>
+                </Card>
+              </Row>
+            )}
+          </Col>
+        </Grid>
+              
+       {this.people.map((value, index) =>
+         <Card>
+         <CardItem button>
+         <Text>
+           WASSUp
+                    </Text>
+         </CardItem>
+       </Card>
+       )}
       </Content>
     );
   }
