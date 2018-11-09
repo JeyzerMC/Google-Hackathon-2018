846ed7a164e70c53e90fb282844e152ad11a4035
Francis Granger <franki_le_pro@hotmail.com>
2018-11-08 20:24:33 -0500

    lol

--- mobileApp/src/App.js
+++ mobileApp/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <Container>
-        <Tabs tabBarPosition="bottom">
+        <Tabs locked={true} tabBarPosition="bottom">
           <Tab heading="Lists">
             <Lists />
           </Tab>
