76528c276d5d807fe9775232e8e52e11c1990c83
Francis Granger <franki_le_pro@hotmail.com>
2018-11-08 19:45:33 -0500

    segments within tabs

--- mobileApp/src/App.js
+++ mobileApp/src/App.js
@@ -1,30 +1,15 @@
 import React, { Component } from "react";
-import { Container, Tabs, Tab, TabHeading, View, Thumbnail } from "native-base";
+import { Container, Tabs, Tab } from "native-base";
 import Lists from "./screens/lists";
 import Primary from "./screens/primary";
 import Delivery from "./screens/delivery";
 
 class App extends Component {
   render() {
-    const uris = {
-      costco: "https://cdn.iconscout.com/icon/free/png-256/costco-282448.png",
-      iga: "https://archive.li/A4yBK/ab49db9d993e090d7e5afabcbb715e8bbf113d84.gif",
-      metro: "https://www.metro.ca/userfiles/image/publicite/logo-metro.png",
-      walmart: "https://cdn.iconscout.com/icon/free/png-256/walmart-282199.png",
-      superC: "https://www.placedeville.com/app/uploads/sites/11/2018/04/SuperC_Web.png",
-      maxi: "https://www.circulaire-en-ligne.ca/wp-content/uploads/Circulaire-en-ligne-Maxi-et-Maxi-Cie.jpg",
-    };
-
     return (
       <Container>
         <Tabs>
-          <Tab heading={
-            <TabHeading>
-              <View>
-                <Thumbnail small source={{ uri: uris.costco }} />
-              </View>
-            </TabHeading>
-          }>
+          <Tab heading="Lists">
             <Lists />
           </Tab>
           <Tab heading="Current">
--- mobileApp/src/screens/lists.js
+++ mobileApp/src/screens/lists.js
@@ -1,44 +1,26 @@
 import React, { Component } from "react";
-import { Content, Card, CardItem, Text, Body } from "native-base";
+import { Content, Button, Thumbnail, Segment } from "native-base";
 
 export default class Lists extends Component {
   render() {
+    const uris = {
+      costco: "https://cdn.iconscout.com/icon/free/png-256/costco-282448.png",
+      iga: "https://archive.li/A4yBK/ab49db9d993e090d7e5afabcbb715e8bbf113d84.gif",
+      metro: "https://www.metro.ca/userfiles/image/publicite/logo-metro.png",
+      walmart: "https://cdn.iconscout.com/icon/free/png-256/walmart-282199.png",
+      superC: "https://www.placedeville.com/app/uploads/sites/11/2018/04/SuperC_Web.png",
+      maxi: "https://www.circulaire-en-ligne.ca/wp-content/uploads/Circulaire-en-ligne-Maxi-et-Maxi-Cie.jpg",
+    };
     return (
-      <Content padder>
-        <Card>
-          <CardItem>
-            <Body>
-              <Text>NativeBase is open source and free.</Text>
-            </Body>
-          </CardItem>
-          <CardItem>
-            <Body>
-              <Text>Platform specific codebase for components</Text>
-            </Body>
-          </CardItem>
-          <CardItem>
-            <Body>
-              <Text>
-                Any native third-party libraries can be included along with
-                NativeBase.
-              </Text>
-            </Body>
-          </CardItem>
-          <CardItem>
-            <Body>
-              <Text>
-                Single file to theme your app and NativeBase components.
-              </Text>
-            </Body>
-          </CardItem>
-          <CardItem>
-            <Body>
-              <Text>
-                Gives an ease to include different font types in your app.
-              </Text>
-            </Body>
-          </CardItem>
-        </Card>
+      <Content>
+        <Segment>
+          <Button first>
+            <Thumbnail large source={{ uri: uris.costco }} />
+          </Button>
+          <Button last>
+            <Thumbnail large source={{ uri: uris.costco }} />
+          </Button>
+        </Segment>
       </Content>
     );
   }
