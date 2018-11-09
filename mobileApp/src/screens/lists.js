166404f5602fa06401a9c50380f6bbd9bd01e6fa
Francis Granger <franki_le_pro@hotmail.com>
2018-11-08 21:31:50 -0500

    clean up

--- mobileApp/src/screens/lists.js
+++ mobileApp/src/screens/lists.js
@@ -5,21 +5,21 @@ export default class Lists extends Component {
   render() {
     const uris = {
       costco: "https://cdn.iconscout.com/icon/free/png-256/costco-282448.png",
-      iga: "https://archive.li/A4yBK/ab49db9d993e090d7e5afabcbb715e8bbf113d84.gif",
+      iga: "https://www.jobillico.com/medias/logo-entreprise/0/0/exp_logo_20974_fr_2018_07_04_10_27_42.jpg",
+      superC: "https://www.placedeville.com/app/uploads/sites/11/2018/04/SuperC_Web.png",
       metro: "https://www.metro.ca/userfiles/image/publicite/logo-metro.png",
       walmart: "https://cdn.iconscout.com/icon/free/png-256/walmart-282199.png",
-      superC: "https://www.placedeville.com/app/uploads/sites/11/2018/04/SuperC_Web.png",
       maxi: "https://www.circulaire-en-ligne.ca/wp-content/uploads/Circulaire-en-ligne-Maxi-et-Maxi-Cie.jpg",
     };
 
     return (
       <Content>
         <Tabs renderTabBar={() => <ScrollableTab />}>
-          {Object.keys(uris).map((key) =>
+          {Object.entries(uris).map(([key, value]) =>
             <Tab key={key} heading={
               <TabHeading>
                 <View>
-                  <Thumbnail small source={{ uri: uris[key] }} />
+                  <Thumbnail small source={{ uri: value }} />
                 </View>
               </TabHeading>
             }>
